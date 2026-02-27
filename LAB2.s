fe44b9d9d14719a6d0a859c83525fdc692bba490
acowaldo <o65@umbc.edu>
2026-02-27 14:55:59 +0000

    Program almost complete, debug hamming distance

--- LAB2.s
+++ LAB2.s
@@ -18,32 +18,38 @@ hammer:
     
 
     #Store strings (USE LEA) 
-    lea STR1, %SI # chuck memory address of STR1 to si
-    lea STR2, %DI #memory address of STR2 to DI
+    lea STR1, %esi # chuck memory address of STR1 to esi
+    lea STR2, %edi #memory address of STR2 to eDI
 
 
     #len two strings, but length of smaller into variable
-    movw $len1, %AX #move length1 into AX
-    cmpw $len2, %AX #Compare string lengths 
+    movb $len1, %al #move length1 into al
+    cmpb $len2, %al #Compare string lengths 
     jl small_len_in_var #jump if len2 smaller than len1
-    
 
+    #get byte amount 
+    #movb $8, %dh
+    #div %dh #divide AX by 8, getting num of bytes
     
-    movw $0, %CX #initialize counter 
-
+    
+    movb $0, %CL #initialize counter 
+    
     #proper length has been aquired (YAY) create loop with comparisons
-    xor_loop: 
-    #does XOR need to be per byte? if do, double for loop, with bytes & 8 rotations
-        #XOR each bit!
-        #movb (%ebx), %ah #move first byte into ah
-        #movb (%edx), %al 
-        #xorb %ah, %al #XORRRRR
+    byte_loop: 
+        #move byte into bh, bl
+        movb (%esi), %bh #move first byte into bh
+        movb (%edi), %bl 
 
-        #jne inc_ham_dist (inequality)
+        xorb %bh, %bl #XORRRRR
+        jne inc_ham_dist #(inequality) #if xor, inequality
+            
+        #shift bitz
+        inc %esi
+        inc %edi 
     
-        inc %CX 
-        cmp %AX, %CX
-        jl xor_loop #if counter != to length 
+        inc %cl
+        cmpb %al, %cl
+        jl byte_loop #if counter != to length 
         ret
 
     #place hamming distance at memory location 0x50
